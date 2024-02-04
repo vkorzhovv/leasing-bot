f7522a82fa930b75ee02b051badce6d11262699f
Vladislav Korzhov <korzhov.vladd@gmail.com>
2024-02-04 23:24:49 +0700

    fix name

--- stories_news/apps.py
+++ stories_news/apps.py
@@ -4,7 +4,7 @@ from django.apps import AppConfig
 class StoriesNewsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "stories_news"
-    verbose_name = "Актуальноые статусы и новости"
+    verbose_name = "Актуальные статусы и новости"
 
     def ready(self):
             import stories_news.signals
